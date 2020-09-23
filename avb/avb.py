Fix free energy calculation based on rederivation of the expression Allow direct optimization of free energy using Adam optimizer

avb/avb.py
@@ -37,15 +37,6 @@ class Avb(LogBase):
         self._debug = kwargs.get("debug", False)
         self._maxits = kwargs.get("max_iterations", 10)
 
-        model_priors = [NormalPrior(p.prior_dist.mean, p.prior_dist.var) for p in self.model.params]
-        noise_prior = NoisePrior(scale=kwargs.get("noise_s0", 1e6), shape=kwargs.get("noise_c0", 1e-6))
-        self.prior = Prior(model_priors, noise_prior)
-        self.post = MVNPosterior(self.data_model, self.model.params, self.tpts)
-
-        self.sess = tf.Session()
-        init = tf.initialize_all_variables()
-        self.sess.run(init)
-        
     def noise_mean_prec(self, dist):
         return dist.noise_c*dist.noise_s, 1/(dist.noise_s*dist.noise_s*dist.noise_c)
 
@@ -136,29 +127,6 @@ class Avb(LogBase):
         else:
             return self.model.params[idx].post_dist.transform.int_values(model_params, ns=tf)
 
-    def _build_graph(self):
-        # Get model prediction and Jacobian from current parameters 
-        self.modelfit, self.J = self._evaluate()        
-        self.Jt = tf.transpose(self.J, (0, 2, 1))
-        self.JtJ = tf.matmul(self.Jt, self.J)
-        self.noise_means, self.noise_precs = self.noise_mean_prec(self.post)
-        self.noise_vars = 1.0/self.noise_precs
-        self.k = self.data - self.modelfit
-        
-        # Here we need to update spatial/ARD priors and capture
-        # their contribution to the free energy
-        
-        # Calculate the free energy and update model parameters
-        # using the AVB update equations
-        self.free_energy = self._free_energy()
-        self.new_means, self.new_cov = self._update_model_params()
-
-        # Follow Fabber in updating residuals after params change before updating 
-        # noise. Note we don't update J and nor does Fabber (until the
-        # end of the parameter updates when it re-centres the linearized model)
-        self.k_new = self.k + tf.einsum("ijk,ik->ij", self.J, self.post.means - self.new_means)
-        self.noise_c_new, self.noise_s_new = self._update_noise()
-       
     def _update_model_params(self):
         """
         Update model parameters
@@ -199,82 +167,157 @@ class Avb(LogBase):
         Linv = self.post.covar
 
         # Calculate individual parts of the free energy
+        # For clarify define the following:
+        c = self.post.noise_c
+        c0 = self.prior.noise_c
+        s = self.post.noise_s
+        s0 = self.prior.noise_s
+        N = tf.cast(self.nt, tf.float32)
+        NP = tf.cast(self.nparam, tf.float32)
+        P = self.post.precs
+        P0 = self.prior.precs
+        C = self.post.covar
+        C0 = self.prior.covar
+        m = self.post.means
+        m0 = self.prior.means
+        from tensorflow.math import log, digamma, lgamma
+        from tensorflow.linalg import matmul, trace, slogdet
 
-        # Bits arising from the factorised posterior for theta
-        expectedLogThetaDist = 0.5 * tf.linalg.slogdet(self.post.precs)[1] - 0.5 * tf.cast(self.nparam, tf.float32) * (tf.math.log(2 * math.pi) + 1)
+        Fparts = []
 
-        # Bits arising fromt he factorised posterior for phi
-        expectedLogPhiDist = -tf.math.lgamma(self.post.noise_c) - self.post.noise_c * tf.math.log(self.post.noise_s) - self.post.noise_c + (self.post.noise_c - 1) * (tf.math.digamma(self.post.noise_c) + tf.math.log(self.post.noise_s))
-
-        # Bits arising from the likelihood
-        expectedLogPosteriorParts = []
-
-        # nTimes using phi_{i+1} = Qis[i].Trace()
-        expectedLogPosteriorParts.append(
-            (tf.math.digamma(self.post.noise_c) + tf.math.log(self.post.noise_s)) * (tf.cast(self.nt, tf.float32) * 0.5 + self.prior.noise_c - 1)
+        # Expected value of the log 
+        #
+        # -0.5sc(KtK + Tr(CJtJ))
+        #
+        # Fabber: 1st term in expectedLogPosteriorParts[2] but missing s*c factor
+        #         2nd term in expectedLogPosteriorParts[2] missing same factor
+        Fparts.append(
+            -0.5*s*c*(tf.reduce_sum(tf.square(self.k), axis=-1) + trace(matmul(C, self.JtJ)))
         )
 
-        expectedLogPosteriorParts.append(
-            -tf.math.lgamma(self.prior.noise_c) - self.prior.noise_c * tf.math.log(self.prior.noise_s) - self.post.noise_s * self.post.noise_c / self.prior.noise_s
+        # Fabber: 1st & 2nd terms in expectedLogPosteriorParts[0]
+        #         3nd term in expectedLogPosteriorParts[3]
+        Fparts.append(
+            0.5*N*(log(s) + digamma(c) - log(2*math.pi))
         )
 
-        expectedLogPosteriorParts.append(
-            -0.5 * tf.reduce_sum(tf.square(self.k), axis=-1) - 0.5 * tf.linalg.trace(tf.matmul(self.JtJ, self.post.covar))
+        # KL divergence of model posterior 
+        #
+        # Fabber: 1st term in expectedLogThetaDist
+        #         2nd term in expectedLogPosteriorParts[5]
+        #         3rd term in expectedLogPosteriorParts[3]
+        #         4th term in expectedLogThetaDist
+        Fparts.append(
+            -0.5*(slogdet(P)[1] + trace(matmul(P0, C))) + 0.5*(slogdet(P0)[1] + NP)
         )
 
-        expectedLogPosteriorParts.append(
-            +0.5 * tf.linalg.slogdet(self.prior.precs)[1]
-            - 0.5 * tf.cast(self.nt, tf.float32) * tf.math.log(2 * math.pi) - 0.5 * tf.cast(self.nparam, tf.float32) * tf.math.log(2 * math.pi)
-        )
-
-        means = self.log_tf(self.post.means, name="means", shape=True, force=False)
-        pmeans = self.log_tf(self.prior.means, name="pmeans", shape=True, force=False)
-        pprecs = self.log_tf(self.prior.precs, name="pprecs", shape=True, force=False)
-        
-        expectedLogPosteriorParts.append(
+        # -0.5 (m-m0)T P0 (m-m0)
+        # Fabber: in expectedLogPosteriorParts[4]
+        Fparts.append(
             -0.5 * tf.reshape(
-              tf.matmul(
-                tf.matmul(
-                    tf.reshape(means - pmeans, (self.nv, 1, self.nparam)), 
-                    pprecs
+              matmul(
+                matmul(
+                    tf.reshape(m - m0, (self.nv, 1, self.nparam)), 
+                    P0
                 ),
-                tf.reshape(means - pmeans, (self.nv, self.nparam, 1))
+                tf.reshape(m - m0, (self.nv, self.nparam, 1))
               ), 
               (self.nv,)
             )
         )
 
-        expectedLogPosteriorParts.append(
-            -0.5 * tf.linalg.trace(self.post.covar * self.prior.precs)
+        # KL divergence of noise posterior from prior
+        # Fabber: 1st term in expectedLogPhiDist
+        #         2nd term in expectedLogPhiDist
+        #         3rd term in expectedLogPhiDist
+        #         4th term in expectedLogPhiDist
+        #         5th term in expectedLogPosteriorParts[0]
+        #         6th term in expectedLogPosteriorParts[9]
+        #         7th term in expectedLogPosteriorParts[9]
+        #         8th term in expectedLogPosteriorParts[9]
+        Fparts.append(
+            -(c-1)*(log(s) + digamma(c)) + c*log(s) + c + lgamma(c) + (c0-1)*(log(s) + digamma(c)) -lgamma(c0) - c0*log(s0) - s*c / s0
         )
 
+        # Fabber: have extra factor of +0.5*Np*log 2PI in expectedLogThetaDist
+        #         have extra factor of -0.5*Np*log 2PI in expectedLogPosteriorParts[3]
+        # these cancel out!
+
         # Assemble the parts into F
-        F = -expectedLogThetaDist - expectedLogPhiDist + sum(expectedLogPosteriorParts)
+        self.Fparts = self.log_tf(tf.stack(Fparts, axis=-1), shape=True, force=False)
+        F = tf.reduce_sum(self.Fparts, axis=-1)
 
         return F
 
-    def run(self, record_history=False):
-        self._build_graph()
+    def _build_graph(self, use_adam, **kwargs):
+        # Set up prior and posterior
+        model_priors = [NormalPrior(p.prior_dist.mean, p.prior_dist.var) for p in self.model.params]
+        noise_prior = NoisePrior(scale=kwargs.get("noise_s0", 1e6), shape=kwargs.get("noise_c0", 1e-6))
+        self.prior = Prior(model_priors, noise_prior)
+        self.post = MVNPosterior(self.data_model, self.model.params, self.tpts, force_positive_vars=use_adam)
+
+        # Get model prediction and Jacobian from current parameters 
+        self.modelfit, self.J = self._evaluate()        
+        self.Jt = tf.transpose(self.J, (0, 2, 1))
+        self.JtJ = tf.matmul(self.Jt, self.J)
+        self.noise_means, self.noise_precs = self.noise_mean_prec(self.post)
+        self.noise_c, self.noise_s = self.post.noise_c, self.post.noise_s
+        self.noise_vars = 1.0/self.noise_precs
+        self.k = self.data - self.modelfit
+        
+        # Here we need to update spatial/ARD priors and capture
+        # their contribution to the free energy
+        
+        # Calculate the free energy and update model parameters
+        # using the AVB update equations
+        self.free_energy = self._free_energy()
+        self.new_means, self.new_cov = self._update_model_params()
+
+        # Follow Fabber in updating residuals after params change before updating 
+        # noise. Note we don't update J and nor does Fabber (until the
+        # end of the parameter updates when it re-centres the linearized model)
+        self.k_new = self.k + tf.einsum("ijk,ik->ij", self.J, self.post.means - self.new_means)
+        self.noise_c_new, self.noise_s_new = self._update_noise()
+       
+        # Define adam optimizer to optimize F directly
+        self.cost = -tf.reduce_mean(self.free_energy)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
+        self.optimize = self.optimizer.minimize(self.cost)
+
+        self.init = tf.global_variables_initializer()
+
+    def run(self, record_history=False, use_adam=False, **kwargs):
+        self._build_graph(use_adam)
 
         self.history = {}
+
+        self.sess = tf.Session()
+        self.sess.run(self.init)
+        
         self._log_iter(0, record_history)
         self._debug_output("Start", self.J)
-        # Update model and noise parameters iteratively
-        for idx in range(self._maxits):
-            self.update_theta = [
-                self.post.means.assign(self.sess.run(self.new_means)),
-                self.post.covar.assign(self.sess.run(self.new_cov))
-            ]
-            self.sess.run(self.update_theta)
-            self._debug_output("Updated theta", self.J) 
-            self.update_noise = [
-                self.post.noise_c.assign(self.sess.run(self.noise_c_new)),
-                self.post.noise_s.assign(self.sess.run(self.noise_s_new))
-            ]
 
-            self.sess.run(self.update_noise)
-            self._debug_output("Updated noise", self.J)
-            self._log_iter(idx+1, record_history)
+        if use_adam:
+            for idx in range(self._maxits):
+                self.sess.run(self.optimize)
+                self._log_iter(idx+1, record_history)
+        else:
+            # Use analytic update equations to update model and noise parameters iteratively
+            for idx in range(self._maxits):
+                self.update_theta = [
+                    self.post.means.assign(self.sess.run(self.new_means)),
+                    self.post.covar_v.assign(self.sess.run(self.new_cov))
+                ]
+                self.sess.run(self.update_theta)
+                self._debug_output("Updated theta", self.J) 
+                self.update_noise = [
+                    self.post.noise_c.assign(self.sess.run(self.noise_c_new)),
+                    self.post.noise_s.assign(self.sess.run(self.noise_s_new))
+                ]
+
+                self.sess.run(self.update_noise)
+                self._debug_output("Updated noise", self.J)
+                self._log_iter(idx+1, record_history)
 
         if record_history:
             for item, item_history in self.history.items():