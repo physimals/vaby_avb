a1c08d37b85ef3425f047f2f545ac8b055b47f5a
Martin Craig <martin.craig@nottingham.ac.uk>
2020-10-13 11:57:51 +0100

    Most support for debug logging

--- avb/posterior.py
+++ avb/posterior.py
@@ -25,8 +25,8 @@ class NoisePosterior(LogBase):
         if np.array(init_c).ndim == 0:
             init_c = tf.fill((nv, ), init_c)
 
-        self.log_s = tf.Variable(tf.math.log(init_s), dtype=tf.float32)
-        self.log_c = tf.Variable(tf.math.log(init_c), dtype=tf.float32)
+        self.log_s = tf.Variable(tf.math.log(init_s), dtype=tf.float32, name="noise_s")
+        self.log_c = tf.Variable(tf.math.log(init_c), dtype=tf.float32, name="noise_c")
         self.s = tf.exp(self.log_s)
         self.c = tf.exp(self.log_c)
 
@@ -95,27 +95,27 @@ class MVNPosterior(LogBase):
         init_mean = tf.stack(init_mean, axis=-1)
         init_var = tf.stack(init_var, axis=-1)
 
-        self.mean = tf.Variable(init_mean, dtype=tf.float32)
+        self.mean = tf.Variable(init_mean, dtype=tf.float32, name="post_mean")
 
         # If we want to optimize this using tensorflow we should build it up as in
         # SVB to ensure it is always positive definite. The analytic approach
         # guarantees this automatically (I think!)
         if kwargs.get("force_positive_var", False):
-            self.log_var = tf.Variable(tf.math.log(init_var))
+            self.log_var = tf.Variable(tf.math.log(init_var), name="post_log_var")
             self.var = tf.math.exp(self.log_var)
             self.std = tf.math.sqrt(self.var)
-            self.std_diag = tf.matrix_diag(self.std)
+            self.std_diag = tf.linalg.diag(self.std)
             cov_init = tf.zeros([nn, len(params), len(params)], dtype=tf.float32)
 
-            self.off_diag_vars = tf.Variable(cov_init)
-            self.off_diag_cov_chol = tf.matrix_set_diag(tf.matrix_band_part(self.off_diag_vars, -1, 0),
+            self.off_diag_vars = tf.Variable(cov_init, name="post_off_diag_cov")
+            self.off_diag_cov_chol = tf.linalg.set_diag(tf.linalg.band_part(self.off_diag_vars, -1, 0),
                                                         tf.zeros([nn, len(params)]))
             self.off_diag_cov_chol = self.log_tf(self.off_diag_cov_chol, shape=True, force=False, name="offdiag")
             self.cov_chol = tf.add(self.std_diag, self.off_diag_cov_chol)
             self.cov = tf.matmul(tf.transpose(self.cov_chol, perm=(0, 2, 1)), self.cov_chol)
             self.cov = self.log_tf(self.cov, shape=True, force=False, name="cov")
         else:
-            self.cov = tf.Variable(tf.linalg.diag(init_var), dtype=tf.float32)
+            self.cov = tf.Variable(tf.linalg.diag(init_var), dtype=tf.float32, name="post_cov")
 
         self.prec = tf.linalg.inv(self.cov)
 
@@ -130,10 +130,10 @@ class MVNPosterior(LogBase):
         prec_new = tf.expand_dims(tf.expand_dims(avb.noise_post.s, -1), -1) * tf.expand_dims(tf.expand_dims(avb.noise_post.c, -1), -1) * avb.JtJ + avb.prior.prec
         cov_new = tf.linalg.inv(prec_new)
 
-        t1 = tf.einsum("ijk,ik->ij", avb.J, self.mean)
+        t1 = self.log_tf(tf.einsum("ijk,ik->ij", avb.J, self.mean), force=False, shape=True, name="t1")
         t15 = tf.einsum("ijk,ik->ij", avb.Jt, (avb.k + t1))
-        t2 = tf.expand_dims(avb.noise_post.s, -1) * tf.expand_dims(avb.noise_post.c, -1) * t15
-        t3 = tf.einsum("ijk,ik->ij", avb.prior.prec, avb.prior.mean)
+        t2 = self.log_tf(tf.expand_dims(avb.noise_post.s, -1) * tf.expand_dims(avb.noise_post.c, -1) * t15, force=False, shape=True, name="t2")
+        t3 = self.log_tf(tf.einsum("ijk,ik->ij", avb.prior.prec, avb.prior.mean), force=False, shape=True, name="t3")
         mean_new = tf.einsum("ijk,ik->ij", cov_new, (t2 + t3))
 
         return [
