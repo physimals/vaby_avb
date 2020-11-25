ef00c9191ad68459b9ff970d974b5a37bc4ed95f
Martin Craig <martin.craig@eng.ox.ac.uk>
2020-11-25 16:42:04 +0000

    ADd combined posterior for MVN output

--- avb/posterior.py
+++ avb/posterior.py
@@ -29,9 +29,12 @@ class NoisePosterior(LogBase):
         self.log_c = tf.Variable(tf.math.log(init_c), dtype=tf.float32, name="noise_c")
         self.s = tf.exp(self.log_s)
         self.c = tf.exp(self.log_c)
+        self.mean = self.c*self.s # [V]
+        self.var = self.s*self.s*self.c # [V]
+        self.prec = 1/self.var # [V]
 
     def mean_prec(self):
-        return self.c*self.s, 1/(self.s*self.s*self.c) # [V], [V]
+        return self.mean, self.prec # [V], [V]
 
     def get_updates(self, avb):
         """
@@ -68,7 +71,8 @@ class MVNPosterior(LogBase):
         """
         """
         LogBase.__init__(self)
-        nn = data_model.n_nodes
+        self.num_nodes = data_model.n_nodes
+        self.num_params = len(params)
 
         init_mean = []
         init_var = []
@@ -84,9 +88,9 @@ class MVNPosterior(LogBase):
                     pass
 
             if mean is None:
-                mean = tf.fill((nn, ), p.post_dist.mean)
+                mean = tf.fill((self.num_nodes, ), p.post_dist.mean)
             if var is None:
-                var = tf.fill((nn, ), p.post_dist.var)
+                var = tf.fill((self.num_nodes, ), p.post_dist.var)
 
             init_mean.append(tf.cast(mean, tf.float32))
             init_var.append(tf.cast(var, tf.float32))
@@ -105,11 +109,11 @@ class MVNPosterior(LogBase):
             self.var = tf.math.exp(self.log_var)
             self.std = tf.math.sqrt(self.var)
             self.std_diag = tf.linalg.diag(self.std)
-            cov_init = tf.zeros([nn, len(params), len(params)], dtype=tf.float32)
+            cov_init = tf.zeros([self.num_nodes, self.num_params, self.num_params], dtype=tf.float32)
 
             self.off_diag_vars = tf.Variable(cov_init, name="post_off_diag_cov")
             self.off_diag_cov_chol = tf.linalg.set_diag(tf.linalg.band_part(self.off_diag_vars, -1, 0),
-                                                        tf.zeros([nn, len(params)]))
+                                                        tf.zeros([self.num_nodes, self.num_params]))
             self.off_diag_cov_chol = self.log_tf(self.off_diag_cov_chol, shape=True, force=False, name="offdiag")
             self.cov_chol = tf.add(self.std_diag, self.off_diag_cov_chol)
             self.cov = tf.matmul(tf.transpose(self.cov_chol, perm=(0, 2, 1)), self.cov_chol)
@@ -140,3 +144,17 @@ class MVNPosterior(LogBase):
             (self.mean, mean_new),
             (self.cov, cov_new),
         ]
+
+class CombinedPosterior(LogBase):
+    """
+    Represents the parameter posterior and the noise posterior as a single
+    distribution for input/output purposes
+    """
+    def __init__(self, post, noise_post):
+        # FIXME in surface mode noise not on nodes and this will not work
+        self.mean = tf.concat([post.mean, tf.reshape(noise_post.mean, (-1, 1))], axis=1)
+
+        cov_model_padded = tf.pad(post.cov, tf.constant([[0, 0], [0, 1], [0, 1]]))
+        cov_noise = tf.reshape(noise_post.var, (-1, 1, 1))
+        cov_noise_padded = tf.pad(cov_noise, tf.constant([[0, 0], [post.num_params, 0], [post.num_params, 0]]))
+        self.cov = cov_model_padded + cov_noise_padded
